437f807219d7d02ea533082b235a33bfbf00a6cc
shashankok7 <61770087+shashankok7@users.noreply.github.com>
2020-03-10 15:23:31 +0530

    2nd commit
    some gameplay added..

---
+++ CubeGame/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 500f;
+    public float sideForce = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce,0,0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce,0,0);
+        }
+
+     
+
+    }
+}
